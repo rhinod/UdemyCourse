b0eda5233c8ae4c8f0a29f9fd2d0378f24de39a5
rhinod <isc.damians@gmail.com>
2020-05-09 09:43:41 -0700

    Create /articles/new view with form

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,4 +8,16 @@ class ArticlesController < ApplicationController
     @articles = Article.all
   end
 
+  def new
+  end
+
+  def create
+    #render plain: params[:article]
+    @article = Article.new(params.require(:article).permit(:title, :description))
+    @article.save
+    @article = Article.last
+    redirect_to article_path(@article)
+    #render plain: @article.inspect
+  end
+
 end
