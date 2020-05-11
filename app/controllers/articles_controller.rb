4d141d14ae056a422e57666facbe8f140a8122cb
rhinod <isc.damians@gmail.com>
2020-05-11 00:03:43 -0700

    Add edit view / Modify routes file

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -12,6 +12,12 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  
+
   def create
     #render plain: params[:article]
     @article = Article.new(params.require(:article).permit(:title, :description))
@@ -23,11 +29,16 @@ class ArticlesController < ApplicationController
     else
       render 'new'
     end
-
-
-
-    
     #render plain: @article.inspect
   end
 
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(params.require(:article).permit(:title,:description))
+      flash[:notice] = "Article updated correctly"
+      redirect_to @article
+    else 
+     render 'edit'
+    end
+  end
 end
