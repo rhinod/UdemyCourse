38e6a3be55a89776beeff34dd9e8da322763f434
rhinod <isc.damians@gmail.com>
2020-05-15 23:47:53 -0700

    Add 1-* association User-Article

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_16_060234) do
+ActiveRecord::Schema.define(version: 2020_05_16_063614) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2020_05_16_060234) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string "author"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
