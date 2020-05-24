cc5e255bb505218df23eb285f89255c53f585ad2
rhinod <isc.damians@gmail.com>
2020-05-23 23:28:25 -0700

    Create category route and write to database

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,8 +1,31 @@
 class CategoriesController < ApplicationController
 
     def new
+      @category = Category.new
     end
 
     def index
     end
+
+    def show
+      @category = Category.findby(params)
+    end
+
+    def create
+        @category = Category.new(category_params)
+
+        if @category.save
+          flash[:notice] = "Categoria hecha"
+          redirect_to @category
+        else
+          render 'new'
+        end
+    end
+
+    private 
+    def category_params
+      params.require(:category).permit(:name)
+    end
+
+
 end
